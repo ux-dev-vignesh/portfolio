b104320ad8db5e19eccc4188ba42a9ebca358f03
ux-dev-vignesh <vigneshperiyasamy.ux@gmail.com>
2024-05-14 20:04:37 +0530

    created-and-config-home-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('../app/pages/home/home.component').then((c) => c.HomeComponent),
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
