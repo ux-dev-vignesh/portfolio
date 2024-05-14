0023d6ab36f50e96793d87e7b7c31545b7b7262a
ux-dev-vignesh <vigneshperiyasamy.ux@gmail.com>
2024-05-14 19:49:20 +0530

    firebase-added-successfully

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideFirestore(() => getFirestore()),
+      provideStorage(() => getStorage())
+    ),
+  ],
 };
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'portfolio-d7d3f',
+    appId: '1:378985927837:web:1141adfb02cdf878f0b074',
+    storageBucket: 'portfolio-d7d3f.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAPSyByQUwj2W6gYkYbBrPVC8aOCLMSqMk',
+    authDomain: 'portfolio-d7d3f.firebaseapp.com',
+    messagingSenderId: '378985927837',
+  },
+};
